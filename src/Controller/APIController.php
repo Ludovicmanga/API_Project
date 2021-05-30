bbb6d70e7cd98ab2227fd4a68518f25142a9c172
Ludovic <ludovic.mangaj@gmail.com>
2021-05-30 13:15:21 +0200

    the displaying of the products list work

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Services\ProductsServiceInterface;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
@@ -13,16 +16,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ApiController extends AbstractController
 {
     /**
-     *@Route("/list", name="list")
+     *@Route("/products/list", name="list")
      */
-    public function list(ProductsServiceInterface $productService): Response
+    public function list(ProductsServiceInterface $productService)
     {
         $products = $productService->findAll();
-        dd($products);
 
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+        $jsonContent = $serializer->serialize($products, 'json', [
+            'circular_reference_handler' => function($object){
+                return $object->getId();
+            }
         ]);
-    }
+        
+        $response = New Response($jsonContent);
 
+        $response->headers->set('Content-type', 'application/json');
+
+        return $response;
+    }
 }
