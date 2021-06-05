910c478bdadc5ec5851b49b7b9e83838833b443f
Ludovic <ludovic.mangaj@gmail.com>
2021-06-05 17:51:58 +0200

    the JWT authentication works!

--- src/Controller/SubscriberController.php
+++ src/Controller/SubscriberController.php
@@ -19,56 +19,5 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     */
 class SubscriberController extends AbstractController
 {
-    private $subscribersService;
-
-    public function __construct(SubscribersServiceInterface $subscribersService)
-    {
-        $this->subscribersService = $subscribersService;
-    }
-
-    /**
-     *@Route("/get/user/{user_id}", 
-     *    name="users_get",
-     *    methods={"GET"})
-     */
-    public function getUserSubscribers(Request $request)
-    {
-        $userId = $request->get('user_id');
-        $subscribers = $this->subscribersService->findByUser($userId);
-
-        return $this->subscribersService->serialize($subscribers); 
-    }
-
-    /**
-     *@Route("/get/{id}", 
-     *    name="get",
-     *    methods={"GET"})
-     */
-    public function getSubscriber(Subscribers $subscriber)
-    {
-        return $this->subscribersService->serialize($subscriber); 
-    }
-
-    /**
-     *@Route("/remove/{id}", 
-     *    name="remove",
-     *    methods={"DELETE"})
-     */
-    public function remove(Subscribers $subscriber)
-    {
-        $this->subscribersService->remove($subscriber);
-    }
-
-    /**
-     *@Route("/create", 
-     *    name="create",
-     *    methods={"POST"})
-     */
-    public function add(Request $request)
-    {
-        //if($request->isXmlHttpRequest()){
-            return $this->subscribersService->createSubscriber($request); 
-        //}
-        //return new Response('Erreur', 404);
-    }
+    
 }
